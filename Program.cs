5e233009110cabea5bacc29964cc0bdbbfe14f5c
a8rill <a8rill@mail.ru>
2024-11-08 22:13:00 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -8,3 +8,32 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string [] newArray (string [] array)
+{
+    string [] result = new string[array.Length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[j] = array[i];
+            j++;
+        }
+    }
+    return result;
+}
+
+void PrintArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = {"Hello", "2", "world", ":-)"};
+string[] resultArray = newArray(array);
+PrintArray(resultArray);
